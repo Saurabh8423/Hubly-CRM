a6510ab6bc31aeb26983404e4e577199e034bffd
saurabh8423 <saurabhch9369@gmail.com>
2025-12-01 06:09:04 +0530

    updated backend url

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,9 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://hubly-crm-two.vercel.app"
+    "https://hubly-crm-two.vercel.app",
   ],
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true
+  credentials: true,
 }));
 
 app.use(helmet());
